bc472210b90efe66b03b94758b8e6d708a2f11e1
Joseph Felker <jayefelker@gmail.com>
2023-05-11 11:43:56 -0500

    render elements on show page

--- src/pages/Show/index.js
+++ src/pages/Show/index.js
@@ -1,25 +1,53 @@
-import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
-import { getQuery } from "../../utilities/queryServices"
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getQuery } from "../../utilities/queryServices";
 
-export default function Show(props){
-    const [query, setQuery] = useState(null)
-    const { id } = useParams()
+export default function Show(props) {
+  const [query, setQuery] = useState(null);
+  const { id } = useParams();
 
- const handleRequest = async () => {
-		try {
-				const queryData = await getQuery(id)
-				setQuery(queryData)
-		}catch(err){
-				console.log(err)
-		}
-}
+  const handleRequest = async () => {
+    try {
+      const queryData = await getQuery(id);
+      setQuery(queryData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  console.log(`Current Query: ${JSON.stringify(query)}`)
+  console.log(`Current Query: ${JSON.stringify(query)}`);
 
   useEffect(() => {
-    handleRequest()
-  }, [])
+    handleRequest();
+  }, []);
 
-  return <h1>Show component</h1>
+  const loaded = () => (
+    <div>
+        <h1>Show Page</h1>
+        <h2>{query.submission}</h2>
+        <h2>{query.response}</h2>
+        <h2>{query.analysis}</h2>
+    </div>
+  )
+
+  const loading = () => {
+    return (
+        <div className="query-list">
+          <h1>
+            Loading...
+            <span>
+              <img
+                className="spinner"
+                src="https://freesvg.org/img/1544764567.png"
+                alt=""
+              />{" "}
+            </span>
+          </h1>
+        </div>
+      );
+  }
+
+
+
+  return query ? loaded() : loading()
 }
