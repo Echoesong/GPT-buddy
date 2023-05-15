ed89fd9eb7c7942564cafe45dbe138f1131b567b
Joseph Felker <jayefelker@gmail.com>
2023-05-14 20:15:51 -0500

    try deploy fix

--- src/pages/Queries/index.js
+++ src/pages/Queries/index.js
@@ -1,36 +1,37 @@
 import { useState, useEffect } from "react";
-import { getQueries, createQuery, deleteQuery } from "../../utilities/queryServices";
-import { Link, useNavigate } from "react-router-dom"
-import { useAuth0 } from "@auth0/auth0-react";
+import {
+  getQueries,
+  createQuery,
+  deleteQuery,
+} from "../../utilities/queryServices";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Queries(props) {
   const [isLoading, setIsLoading] = useState(true);
   const [queries, setQueries] = useState([]);
-  const [refreshData, setRefreshData] = useState(false)
+  const [refreshData, setRefreshData] = useState(false);
   const navigate = useNavigate();
-  
-//   Below I set state for a form. For my app, the only thing they will input is the text submission; response and analysis will be filled in by GPT response
+
+  //   Below I set state for a form. For my app, the only thing they will input is the text submission; response and analysis will be filled in by GPT response
   const [newForm, setNewForm] = useState({
-    submission: '',
-    response: '',
-    analysis: ''
-  })
+    submission: "",
+    response: "",
+    analysis: "",
+  });
   const handleChange = (e) => {
-    e.preventDefault()
-    setNewForm({...newForm, [e.target.name]: e.target.value})
-  }
+    e.preventDefault();
+    setNewForm({ ...newForm, [e.target.name]: e.target.value });
+  };
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     // eslint-disable-next-line no-unused-vars
-    const newQuery = await createQuery(newForm)
-    setNewForm({submission: '', response: '', analysis: '' })
-    handleRequest()
-  }
-  const { getAccessTokenSilently } = useAuth0()
+    const newQuery = await createQuery(newForm);
+    setNewForm({ submission: "", response: "", analysis: "" });
+    handleRequest();
+  };
   async function handleRequest() {
     try {
-      const token = await getAccessTokenSilently()
-      const queriesData = await getQueries(token);
+      const queriesData = await getQueries();
       setQueries(queriesData);
       setIsLoading(false);
     } catch (err) {
@@ -39,28 +40,27 @@ export default function Queries(props) {
   }
 
   const handleQueryDelete = async (id) => {
-    console.log('id passed to dlete func', id)
-    try{
-      const delResponse = await deleteQuery(id)
-      console.log(delResponse)
+    console.log("id passed to dlete func", id);
+    try {
+      const delResponse = await deleteQuery(id);
+      console.log(delResponse);
 
-      if(delResponse._id){
-        setRefreshData(!refreshData)
-      } else{
-        console.log('delResponse:', delResponse)
-        throw new Error('Something went wrong')
+      if (delResponse._id) {
+        setRefreshData(!refreshData);
+      } else {
+        console.log("delResponse:", delResponse);
+        throw new Error("Something went wrong");
       }
-    } catch(err){
-      console.log(err)
-      navigate(`/queries/${id}`)
+    } catch (err) {
+      console.log(err);
+      navigate(`/queries/${id}`);
     }
-
-  }
+  };
 
   useEffect(() => {
     handleRequest();
   }, [refreshData]);
-  console.log("all shown queries", queries)
+  console.log("all shown queries", queries);
   const loaded = () => {
     if (Array.isArray(queries)) {
       return queries.map((query) => {
@@ -95,8 +95,6 @@ export default function Queries(props) {
       return null;
     }
   };
-  
-  
 
   const loading = () => {
     return (
@@ -117,26 +115,26 @@ export default function Queries(props) {
 
   return (
     <div className="border-grey-400">
-        
-        <section className='flex flex-col sm:flex-row justify-center items-center'>
+      <section className="flex flex-col sm:flex-row justify-center items-center">
+        <h2 className="text-center">Enter prompt here</h2>
+        <form onSubmit={handleSubmit} className="">
+          <input
+            className="w-full sm:w-auto rounded-lg border mb-4 border-gray-300 px-4 py-2 mx-1"
+            onChange={handleChange}
+            type="text"
+            value={newForm.submission}
+            name="submission"
+            placeholder="'QUESTION' or 'SUMMARY''"
+          />
 
-            <h2 className='text-center'>Enter prompt here</h2>
-            <form onSubmit={handleSubmit} className='' >
-            <input
-  className='w-full sm:w-auto rounded-lg border mb-4 border-gray-300 px-4 py-2 mx-1'
-  onChange={handleChange}
-  type="text"
-  value={newForm.submission}
-  name="submission"
-  placeholder="'QUESTION' or 'SUMMARY''"
-/>
-
-                    <button
-                    className='btn btn-primary my-5 mx-1 px-2 rounded-lg border border-gray-300 hover:bg-green-300'
-                    >Send query</button>
-            </form>
-        </section>
-        <section className="grid grid-cols-1 justify-items-center gap-4">{isLoading ? loading() : loaded()}</section>
+          <button className="btn btn-primary my-5 mx-1 px-2 rounded-lg border border-gray-300 hover:bg-green-300">
+            Send query
+          </button>
+        </form>
+      </section>
+      <section className="grid grid-cols-1 justify-items-center gap-4">
+        {isLoading ? loading() : loaded()}
+      </section>
     </div>
   );
 }
