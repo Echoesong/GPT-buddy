f953aed40d3b3bc8c9a3ff60940f203f6ad0dd8e
Joseph Felker <jayefelker@gmail.com>
2023-05-22 15:56:23 -0500

    change styling for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Nav(){
 
     return (
-      <nav className="bg-gray-800 text-white shadow-lg">
+      <nav className="flex basis-0 flex-row-reverse bg-gray-800 text-white shadow-lg px-1">
         <Link
           to="/"
           className="bg-gray-300 my-2 inline-block p-2 rounded-full hover:bg-blue-800"
