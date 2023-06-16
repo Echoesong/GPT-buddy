76a4869ebe422e174ae9450dc22e5facf5e0b822
Joseph Felker <jayefelker@gmail.com>
2023-06-16 14:46:55 -0500

    add Auth0 login logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,36 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "../Auth0/login";
+import LogoutButton from "../Auth0/logout"; 
 
-function Nav(){
+function Nav() {
+  const { isAuthenticated } = useAuth0();
 
-    return (
-      <header className="mb-2">
+  return (
+    <header className="mb-2">
       <div className="navbar flex justify-between border-r-2 bg-gray-800">
         <div className="flex">
           <Link to="/">
-            <div className="btn btn-outline normal-case txt-xl mx-1 ">Home</div>
+            <div className="btn btn-primary normal-case txt-xl mx-1 ">Home</div>
           </Link>
           <Link to="/about">
-            <div className="btn btn-outline normal-case txt-xl mx-1 ">About</div>
+            <div className="btn btn-primary normal-case txt-xl mx-1 ">
+              About
+            </div>
           </Link>
-
-          
+        </div>
+        <div className="btn btn-primary normal-caste txt-xl mx-1 flex flex-row-reverse">
+          {isAuthenticated ? <LogoutButton /> : <LoginButton />}
         </div>
       </div>
     </header>
-    );
-    
+  );
 }
 
-export default function Header(props){
-    return (
-        <div className='Header'>
-            <Nav/>
-            
-        </div>
-    )
+export default function Header(props) {
+  return (
+    <div className="Header">
+      <Nav />
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './App.css'
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  
-
-  
-  <Router>
-    <App />
-  </Router>
-  
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
+    <Router>
+      <App />
+    </Router>
+  </Auth0Provider>,
 );
-
